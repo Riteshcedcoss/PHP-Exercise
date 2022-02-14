5f2dde5f28d9e40230279a22c1d3c7218cd1bbd0
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-14 17:05:28 +0530

    Task 6

--- src/index.php
+++ src/index.php
@@ -31,7 +31,35 @@ else{
 }
 //fifth
 $message = "Welcome to the PHP World";
-echo $message;
+echo $message,"<br>";
+//six
+$day = "4";
+
+switch ($day) {
+    case "1":
+        echo "It is Monday!";
+        break;
+    case "2":
+        echo "It is today!";
+        break;
+    case "3":
+        echo "It is Wednesday!";
+        break;
+	case "4":
+        echo "It is Thursday!";
+        break;
+    case "5":
+        echo "It is Friday!";
+        break;
+    case "6":
+        echo "It is Saturday!";
+        break;
+	case "7":
+        echo "It is Sunday!";
+        break;
+    default:
+        echo "Invalid number!";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
